7bf7776171b173cc36e2648580ded258753184ab
Aymeric Hermann <42406509+tupuni@users.noreply.github.com>
2023-01-28 00:02:59 +0100

    Update exemple_ggplot.R

--- workshop_scripts/exemple_ggplot.R
+++ workshop_scripts/exemple_ggplot.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 
+# creer d'un dataframe hypothetique 
 dataframe <- data.frame(
   artefact_id = seq(1,87),
   longueur = rnorm(n=87, mean=50, sd=10),
@@ -11,16 +12,21 @@ dataframe <- data.frame(
                             nrow(dataframe), replace = TRUE)
   )
 
+# enregistrer le theme que l'on veut utiliser dans ggplot
 my_theme <- theme_classic() + 
   theme(axis.line=element_blank()) +
   theme(panel.border = element_rect(colour="black", fill = NA, size = .75),
         legend.position="none", aspect.ratio=1)
 
+# un premier plot L/l
 dataframe %>% 
   ggplot(aes(x = longueur, y = largeur)) +
   geom_point() +
   my_theme
   
+# declinaison de ce plot L/l par categorie :
+# par eclats bruts et retouches
+# et par matiere premiere A et B
 dataframe %>% 
   ggplot(aes(x = longueur, y = largeur)) +
   geom_point() +
