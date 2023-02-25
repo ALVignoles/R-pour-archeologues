36f99b39da4b87e3e688d85f52345e28b057af4e
Tupuni <aymeric.hermann@cnrs.fr>
2023-02-25 10:03:03 +0100

    adds Seance2.R and Seance2.md

--- workshop_scripts/Seance2.R
+++ workshop_scripts/Seance2.R
@@ -1,6 +1,22 @@
+#### Introduction ####
+# Au cours de cette seance nous utiliserons l'extension (package) tidyverse.
+# Le terme tidyverse est une contraction de 'tidy' (qu’on pourrait traduire par “bien rangé”) et de 'universe'.
+# Il s’agit en fait d’une collection d’extensions conçues pour travailler ensemble et basées sur une philosophie commune.
+# Elles abordent un très grand nombre d’opérations courantes dans R (la liste n’est pas exhaustive) :
+  
+  visualisation
+manipulation des tableaux de données
+import/export de données
+manipulation de variables
+extraction de données du Web
+programmation
+
+Un des objectifs de ces extensions est de fournir des fonctions avec une syntaxe cohérente, qui fonctionnent bien ensemble, et qui retournent des résultats prévisibles. Elles sont en grande partie issues du travail d’Hadley Wickham, qui travaille désormais pour RStudio.
+
 library(tidyverse)
 
-# creer d'un dataframe hypothetique 
+
+# creation d'un dataframe hypothetique 
 dataframe <- data.frame(
   artefact_id = seq(1,87),
   longueur = rnorm(n=87, mean=50, sd=10),
