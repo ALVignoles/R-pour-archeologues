aa30a5e99c3f08912a093e8e8ba78c4efd119a7c
Anaïs L. Vignoles <77483779+ALVignoles@users.noreply.github.com>
2023-03-30 13:20:19 +0200

    update TP

--- workshop_preparation/TPs-réponses.R
+++ workshop_preparation/TPs-réponses.R
@@ -1,10 +1,7 @@
-install.packages("archdata")
-library(archdata)
-
-# TP 1 :
+# TP 1 ####
 ## 1.
 ### 1. 
-install.packages("archdata")
+#install.packages("archdata")
 library(archdata)
 
 ### 2. 
@@ -16,14 +13,19 @@ data("DartPoints")
 ## 2.
 ### 1. 
 DartPoints
-ncol(DartPoints)
-nrow(DartPoints)
+ncol(DartPoints) # 17
+nrow(DartPoints) # 91
 
-# TP 2 : visualiser un jeu de donnÃ©es 
+### 2.
+summary(DartPoints) 
+class(DartPoints$Name) #character, numeric, factor : 3 
+
+
+# TP 2 : visualiser un jeu de données #### 
 data("DartPoints")
 
 
-# TP 3 : faire des boucles 
+# TP 3 : faire des boucles ####
 data("BarmoseI.pp")
 data(BarmoseI.grid)
 plot(BarmoseI.grid$East, BarmoseI.grid$North)
@@ -31,5 +33,5 @@ points(BarmoseI.pp$East, BarmoseI.pp$North, col = BarmoseI.pp$Class)
 
 data("Snodgrass")
 
-# TP 4 : SIG avec R
+# TP 4 : SIG avec R ####
 data("Acheulean")
