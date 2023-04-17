07c2d1f2af65db9fa32b7a03655607e83f536241
Anaïs L. Vignoles <77483779+ALVignoles@users.noreply.github.com>
2023-04-17 15:04:29 +0200

    maj TP
    voilà, il ne manque que la partie 3 du TP sur la visualisation. Je m'en occupe tout à l'heure

--- workshop_preparation/TPs-réponses.R
+++ workshop_preparation/TPs-réponses.R
@@ -1,4 +1,4 @@
-# TP 1 ####
+# TP 1 & 2 ####
 ## 1.
 ### 1. 
 #install.packages("archdata")
@@ -21,9 +21,38 @@ summary(DartPoints)
 class(DartPoints$Name) 
 class(DartPoints$Length) #character, numeric, factor : 3 
 
+### 3.
+Darl <- DartPoints[DartPoints$Name == "Darl",] # exemple avec le premier type 
+summary(Darl)
 
-# TP 2 : visualiser un jeu de donn?es #### 
-data("DartPoints")
+#boucle pour obtenir un tableau final 
+df <- data.frame(levels(DartPoints$Name)) #create df with points types for each row
+colnames(df) <- c("Types")
+
+df$Length_Median <- NA #initialise col for each stat
+df$Length_Range <- NA
+df$Width_Median <- NA
+df$Width_Range <- NA
+df$Thickness_Median <- NA
+df$Thickness_Range <- NA
+
+for(i in 1:nrow(df)){
+  type <- DartPoints[DartPoints$Name == levels(DartPoints$Name)[i], ]#retrieving data for each type
+  
+  df$Length_Median[i] <- median(type$Length) 
+  df$Length_Range[i] <- max(type$Length) - min(type$Length)
+  df$Width_Median[i] <- median(type$Width) 
+  df$Width_Range[i] <- max(type$Width) - min(type$Width)
+  df$Thickness_Median[i] <- median(type$Thickness) 
+  df$Thickness_Range[i] <- max(type$Thickness) - min(type$Thickness)
+  
+  i <- i + 1
+}
+df #tableau de résultats
+
+
+## 3.
+### 1. 
 
 
 # TP 3 : faire des boucles ####
